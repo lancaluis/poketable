24fd2147290491ec6e648dfa0213bb21dda5ac14
Luís Lança <luis_lanca@outlook.com>
2024-09-07 23:48:04 +0300

    chore(datatable): adding scroll action inside the table

--- src/components/DataTable/index.tsx
+++ src/components/DataTable/index.tsx
@@ -40,55 +40,57 @@ export default function DataTable() {
 
   return (
     <div className="mx-auto w-full max-w-4xl">
-      <Table className="min-w-full rounded-lg bg-white shadow-md">
-        <TableHeader>
-          <TableRow className="bg-yellow-400">
-            <TableHead className="px-6 py-3 text-left text-sm font-semibold uppercase tracking-wider">
-              Name
-            </TableHead>
-            <TableHead className="px-6 py-3 text-left text-sm font-semibold uppercase tracking-wider">
-              Image
-            </TableHead>
-            <TableHead className="px-6 py-3 text-left text-sm font-semibold uppercase tracking-wider">
-              Types
-            </TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {isLoading ? (
-            <DataTableSkeleton />
-          ) : (
-            data?.details?.map((details) => (
-              <TableRow
-                key={details.name}
-                className="odd:bg-blue-100 even:bg-blue-50 hover:bg-blue-200"
-              >
-                <TableCell className="px-6 py-1 text-sm font-medium capitalize text-gray-900">
-                  {details.name}
-                </TableCell>
-                <TableCell className="px-6 py-1 text-sm font-medium text-gray-900">
-                  <Image
-                    src={details.sprites.front_default}
-                    alt={details.name}
-                    width={60}
-                    height={60}
-                  />
-                </TableCell>
-                <TableCell className="px-5 py-1 text-sm font-medium text-gray-900">
-                  {details.types.map((type) => (
-                    <span
-                      key={type.slot}
-                      className="mx-1 rounded bg-white px-3 py-1 text-sm"
-                    >
-                      {type.type.name}
-                    </span>
-                  ))}
-                </TableCell>
-              </TableRow>
-            ))
-          )}
-        </TableBody>
-      </Table>
+      <div className="max-h-[30rem] overflow-y-auto">
+        <Table className="min-w-full rounded-lg bg-white shadow-md">
+          <TableHeader>
+            <TableRow className="bg-yellow-400">
+              <TableHead className="px-6 py-3 text-left text-sm font-semibold uppercase tracking-wider">
+                Name
+              </TableHead>
+              <TableHead className="px-6 py-3 text-left text-sm font-semibold uppercase tracking-wider">
+                Image
+              </TableHead>
+              <TableHead className="px-6 py-3 text-left text-sm font-semibold uppercase tracking-wider">
+                Types
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {isLoading ? (
+              <DataTableSkeleton />
+            ) : (
+              data?.details?.map((details) => (
+                <TableRow
+                  key={details.name}
+                  className="odd:bg-blue-100 even:bg-blue-50 hover:bg-blue-200"
+                >
+                  <TableCell className="px-6 py-1 text-sm font-medium capitalize text-gray-900">
+                    {details.name}
+                  </TableCell>
+                  <TableCell className="px-6 py-1 text-sm font-medium text-gray-900">
+                    <Image
+                      src={details.sprites.front_default}
+                      alt={details.name}
+                      width={60}
+                      height={60}
+                    />
+                  </TableCell>
+                  <TableCell className="px-5 py-1 text-sm font-medium text-gray-900">
+                    {details.types.map((type) => (
+                      <span
+                        key={type.slot}
+                        className="mx-1 rounded bg-white px-3 py-1 text-sm"
+                      >
+                        {type.type.name}
+                      </span>
+                    ))}
+                  </TableCell>
+                </TableRow>
+              ))
+            )}
+          </TableBody>
+        </Table>
+      </div>
       {data?.details && (
         <DataTablePagination
           data={data}
